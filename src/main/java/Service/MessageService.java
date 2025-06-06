c1346d144e03c87e01e267624b6490dda0e6c5f3
Zhuo Feng Situ <situzhuofeng@yahoo.com>
2025-06-06 01:59:12 +0000

    Create MessageService class

---
+++ src/main/java/Service/MessageService.java
@@ -0,0 +1,24 @@
+package Service;
+
+import Model.Account;
+import Model.Message;
+import DAO.MessageDAO;
+
+import java.util.List;
+
+public class MessageService {
+    private MessageDAO messageDAO;
+
+    public MessageService(){
+        messageDAO = new MessageDAO();
+    }
+
+    public MessageService(MessageDAO messageDAO){
+        this.messageDAO = messageDAO;
+    }
+
+    public Message createMessage(Message message){
+        
+    }
+    
+}
